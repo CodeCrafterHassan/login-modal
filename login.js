9463e445e6d9e6ef4f1e6ce8e30f3e3d901906d2
Code_Crafter <webcodecrafter1@gmail.com>
2025-04-10 23:06:40 +0500

    Add files via upload

---
+++ login.js
@@ -0,0 +1,32 @@
+let btn = document.querySelectorAll('.login-btn');
+let btnOuter =document.querySelector('.login-btn')
+let movingDiv = document.querySelectorAll('.moving-color');
+let outerModal = document.querySelector('.outer');
+let btnInner = document.querySelector('.modal-btn');
+
+btn.forEach(function(evt){
+    evt.addEventListener('mousemove', function(e){
+        let rect = evt.getBoundingClientRect();
+        let x = e.clientX - rect.left;
+        let y = e.clientY - rect.bottom;
+        movingDiv.forEach(function(el){
+            el.style.transform = ` translateX(${x - '75'}px) translateY(${y - '35'}px)`
+        })
+    })
+});
+btnOuter.addEventListener('click',function(){
+    outerModal.classList.add('open');
+    btnOuter.style.display = "none";
+})
+btnInner.addEventListener('click', function(el){
+    const userName = document.querySelector('.username').value;
+    const password = document.querySelector('.password').value;
+    alert(`Entered Username: ${userName}
+                Password: ${password}`);
+})
+window.addEventListener('click' , function(e){
+    if (e.target === outerModal){
+        outerModal.classList.remove('open')
+        btnOuter.style.display = "flex";
+    }
+})
