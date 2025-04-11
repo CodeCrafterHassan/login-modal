dae2f214d9544a05e71925f7f41a26c89449c393
Code_Crafter <webcodecrafter1@gmail.com>
2025-04-11 15:12:45 +0500

    Update login.js

--- login.js
+++ login.js
@@ -3,6 +3,10 @@ let btnOuter =document.querySelector('.login-btn')
 let movingDiv = document.querySelectorAll('.moving-color');
 let outerModal = document.querySelector('.outer');
 let btnInner = document.querySelector('.modal-btn');
+let eyeBtn = document.querySelector('.eye')
+let hideBtn = document.querySelector('.hide');
+let showBtn = document.querySelector('.show');
+let passwordType = document.querySelector('.password')
 
 btn.forEach(function(evt){
     evt.addEventListener('mousemove', function(e){
@@ -14,6 +18,7 @@ btn.forEach(function(evt){
         })
     })
 });
+
 btnOuter.addEventListener('click',function(){
     outerModal.classList.add('open');
     btnOuter.style.display = "none";
@@ -30,22 +35,28 @@ window.addEventListener('click' , function(e){
         btnOuter.style.display = "flex";
     }
 })
-btn.forEach(function(evt) {
-  // Touch move (swipe inside button)
-  evt.addEventListener('touchmove', function(e) {
-    e.preventDefault(); // Prevent scrolling
-
-    let rect = evt.getBoundingClientRect();
+    eyeBtn.addEventListener('click', function(){
+        if(hideBtn.style.opacity === '1'){
+            hideBtn.style.opacity = '0'
+            showBtn.style.opacity = '1'
+            passwordType.type = 'text'
+        }else{
+            hideBtn.style.opacity = '1'
+            showBtn.style.opacity = '0'
+            passwordType.type = 'password'
+        }
+    })
+btn.forEach(function(evt){
+    evt.addEventListener('mousemove', function(e){
+    e.preventDefault()
+    let move = e.getBoundingClientRect();
     let touch = e.touches[0];
+    let x = touch.clientX - move.left;
+    let y = touch.clientY - move.top;
 
-    // Get position of touch relative to the button
-    let x = touch.clientX - rect.left;
-    let y = touch.clientY - rect.top; // use top instead of bottom
+    movingDiv.forEach(function(e){
+        e.style.transform = `translateX(${x -'75'}px) translateY(${y -'35'}px)`
+    })
 
-    // Move the div relative to touch inside the button
-    movingDiv.forEach(function(el) {
-      el.style.transform = `translateX(${x - 75}px) translateY(${y - 35}px)`;
-    });
-  }, { passive: false });
-
-});
+    })
+})
